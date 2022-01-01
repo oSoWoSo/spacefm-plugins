6b7c1cd59252deeac65a70b54374bf8922aefe71
zenobit <6384793+zen0bit@users.noreply.github.com>
2022-01-01 16:19:47 +0100

    Update _naarGitHub.sh

--- _naarGitHub.sh
+++ _naarGitHub.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-## pak al deze spacefm plugins uit naar $basis om op te laden naar github
-basis=/media/ramdisk
+## unpack all these spacefm plugins to $base to upload to github
+base=/media/ramdisk
 for z in *.spacefm-plugin.tar.gz *.spacefm-file-handler.tar.gz
 do
-	doeldir="$basis"/spacefm-plugins/"${z/.tar.gz}"
-	mkdir -p "$doeldir"
-	tar -xf "$z" -C "$doeldir"
+targetdir="$base"/spacefm-plugins/"${z/.tar.gz}"
+mkdir -p "$targetdir"
+tar -xf "$z" -C "$targetdir"
 done
 pushd "$basis"/spacefm-plugins
-echo "# De hele verzameling">"$basis"/collectie.md
+echo "# The whole collection">"$base"/collection.md
 for d in *.spacefm-plugin *.spacefm-file-handler
 do
-	md="${d/.spacefm-*}.md"
-	echo -e "# $d" > "$md"
-	sed -nE 's/^(cstm|hand_f)_.*-label=(.*)/## \2\n    /p;s/^(cstm|hand_f)_.*-line=/    /p' "$d"/plugin | sed -E 's/\\n/\n    /g;s/\\t/\t/g' >> "$md"
-	cat "$md">>"$basis"/collectie.md
-	echo "">>"$basis"/collectie.md
+md="${d/.spacefm-*}.md"
+echo -e "# $d" > "$md"
+sed -nE 's/^(cstm|hand_f)_.*-label=(.*)/## \2\n /p;s/^(cstm|hand_f)_.*-line=/ /p' "$d"/plugin | sed -E 's/\\n/\n /g;s/\\t/\t/g' >> "$md"
+cat "$md">>"$base"/collection.md
+echo "">>"$base"/collection.md
 done
 popd
-# verwijder export van alle plugins ouder dan dit script (maar collectie.nd blijft volledig)
-find -maxdepth 1 \( -name "*.spacefm-plugin.tar.gz" -o -name "*.spacefm-file-handler.tar.gz" \) \! -newer "_naarGitHub.sh" -exec bash -v -c "cd $basis/spacefm-plugins;"'rm -R "${1/.spacefm-*}.md" "${1/.tar.gz}/"' _ \{\} \;
-# markeer NU als nieuwe scheidslijn voor gewijzigde plugins
-touch "$0"
+# remove export of all plugins older than this script (but collection.nd remains complete)
+find -maxdepth 1 \( -name "*.spacefm-plugin.tar.gz" -o -name "*.spacefm-file-handler.tar.gz" \) \! -newer "_toGitHub.sh" -exec bash -v -c "cd $basis/spacefm-plugins;"'rm -R "${1/.spacefm-*}.md" "${1/.tar.gz }/"' _ \{\} \;
+# mark NOW as new dividing line for changed plugins
+touch "$0" 
